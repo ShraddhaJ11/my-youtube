0f152e6fd784ce548c2ebc4fa027107ce42d6d7f
ShraddhaJ11 <shraddhaj1103@gmail.com>
2023-02-11 20:45:33 +0530

    fix: small fix

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,3 @@
 const GOOGLE_API_KEY = 'AIzaSyCfXXzU7wimn1HnC_vecAn1ss7SsODDyUs';
-// 'AIzaSyA4i6zOV2ioetNMfRROwA76TaP5CISAMHQ';
 
 export const YOUTUBE_VIDEO_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=${GOOGLE_API_KEY}`;
